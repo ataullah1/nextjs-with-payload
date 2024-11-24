898d25a13c162589b243185e922eea78b1fe2c53
Md Ataullah <ataullahm100@gmail.com>
2024-11-24 14:34:13 +0600

    Implement nextjs with payload CMS

---
+++ src/app/(frontend)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-900 text-white">
+      <div className="min-h-screen w-full flex items-center justify-center ">
+        <h1 className="text-4xl font-bold">Hello</h1>
+      </div>
+    </div>
+  )
+}
